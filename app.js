e50236f75f7f04ffa8ac18ee11423b315f169f0e
behroozebrazi <behber@berkeley.edu>
2025-12-31 15:18:33 -0800

    initialize script

---
+++ app.js
@@ -0,0 +1,35 @@
+
+let cart = []
+
+// Receive product
+class Product {
+
+}
+
+// Show products in DOM
+class View {
+
+}
+
+// Storage products
+class Storage {
+
+}
+
+
+// Create objects after loading DOM
+document.addEventListener('DOMContentLoaded', (e) => {
+  const product = new Product()
+  const view = new View()
+  const storage = new Storage()
+
+
+
+
+
+})
+
+
+
+
+
